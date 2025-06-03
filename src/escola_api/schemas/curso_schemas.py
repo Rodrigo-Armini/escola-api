2a5afc7addf83566291d45a676046a0f69703cdc
Rodrigo Novaes Armini <rodrigoarmini@gmail.com>
2025-06-02 21:03:00 -0300

    Separado em rotas

---
+++ src/escola_api/schemas/curso_schemas.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+
+
+
+class Curso(BaseModel):
+    id: int = Field()
+    nome: str = Field()
+    sigla: Optional[str] = Field(default=None)
+
+
+class CursoCadastro(BaseModel):
+    nome: str = Field()
+    sigla: str = Field()
+
+
+class CursoEditar(BaseModel):
+    nome: str = Field()
+    sigla: str = Field()
